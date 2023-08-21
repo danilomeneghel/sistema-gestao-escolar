8a91cba6dc8e9862e72bcfc42f99554d98f9bf66
Danilo Meneghel <danilo.meneghel@gmail.com>
2023-08-20 21:21:56 -0300

    Atualização de arquivos

--- resources/views/alunos/edit.blade.php
+++ resources/views/alunos/edit.blade.php
@@ -4,15 +4,15 @@
 
  @section('content')
 
-  <p class="h2">Novo Aluno</p>
+  <p class="h2">Editar Aluno</p>
   <div class="d-grid gap-2 mb-3 d-md-block">
      <a href="{{route('alunos.index')}}" class="btn btn-outline-secondary" role="button">Voltar</a>
   </div>
 
   <form method="POST" action="{{route('alunos.update', $aluno)}}" class="row g-3">
     @method('PUT')
-    
+
     @include('alunos._form')
-  
+
   </form>
  @endsection
--- resources/views/escolas/edit.blade.php
+++ resources/views/escolas/edit.blade.php
@@ -4,15 +4,15 @@
 
  @section('content')
 
-  <p class="h2">Novo Escola</p>
+  <p class="h2">Editar Escola</p>
   <div class="d-grid gap-2 mb-3 d-md-block">
      <a href="{{route('escolas.index')}}" class="btn btn-outline-secondary" role="button">Voltar</a>
   </div>
 
   <form method="POST" action="{{route('escolas.update', $escola)}}" class="row g-3">
     @method('PUT')
-    
+
     @include('escolas._form')
-  
+
   </form>
  @endsection
